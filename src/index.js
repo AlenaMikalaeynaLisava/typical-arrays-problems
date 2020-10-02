7bc649af366654765e110b8d0b995bdab04e59af
AlenaMikalaeynaLisava <63248586+AlenaMikalaeynaLisava@users.noreply.github.com>
2020-10-02 17:31:54 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 
 exports.min = function min (array) {
-    if (Array.isArray(array)) {
-
       let variable = array[0];
   array.forEach((element) => {
     if (variable < element) {
@@ -9,13 +7,9 @@ exports.min = function min (array) {
     }
   });
   return variable;
-
-  } else return 0;
 }
 
 exports.max = function max (array) {
-  if (Array.isArray(array)) {
-    if (array.length !== 0) {
       let variable = array[0];
   array.forEach((element) => {
     if (variable > element) {
@@ -23,18 +17,12 @@ exports.max = function max (array) {
     }
   });
   return variable;
-    } else return 0;
-  } else return 0;
 }
 
 exports.avg = function avg (array) {
-  if (Array.isArray(array)) {
-    if (array.length !== 0) {
       let sum = 0;
       array.forEach((element) => {
         sum += element;
       });
       return sum / array.length;
-    } else return 0;
-  } else return 0;
 }
