6616a5023ff0f843fedd4a3180cfa967e9e19bb0
AlenaMikalaeynaLisava <alenalisava87@gmail.com>
2020-10-02 17:47:42 +0300

    feat: add all functions

--- src/index.js
+++ src/index.js
@@ -1,28 +1,39 @@
+exports.min = function min(array) {
+    if (Array.isArray(array)) {
+        if (array.length !== 0) {
+            let variable = array[0];
+            array.forEach((element) => {
+                if (variable < element) {
+                    variable = element;
+                }
+            });
+            return variable;
+        } else return 0;
+    } else return 0;
+};
 
-exports.min = function min (array) {
-      let variable = array[0];
-  array.forEach((element) => {
-    if (variable < element) {
-      variable = element;
-    }
-  });
-  return variable;
-}
+exports.max = function max(array) {
+    if (Array.isArray(array)) {
+        if (array.length !== 0) {
+            let variable = array[0];
+            array.forEach((element) => {
+                if (variable > element) {
+                    variable = element;
+                }
+            });
+            return variable;
+        } else return 0;
+    } else return 0;
+};
 
-exports.max = function max (array) {
-      let variable = array[0];
-  array.forEach((element) => {
-    if (variable > element) {
-      variable = element;
-    }
-  });
-  return variable;
-}
-
-exports.avg = function avg (array) {
-      let sum = 0;
-      array.forEach((element) => {
-        sum += element;
-      });
-      return sum / array.length;
-}
+exports.avg = function avg(array) {
+    if (Array.isArray(array)) {
+        if (array.length !== 0) {
+            let sum = 0;
+            array.forEach((element) => {
+                sum += element;
+            });
+            return sum / array.length;
+        } else return 0;
+    } else return 0;
+};
