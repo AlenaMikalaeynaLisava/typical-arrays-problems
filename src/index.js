33e302e6b3d49d877704d61004c3f1737a8203ed
Gemorroj <wapinet@mail.ru>
2020-10-02 18:16:54 +0300

    example

--- src/index.js
+++ src/index.js
@@ -1,39 +1,50 @@
 exports.min = function min(array) {
-    if (Array.isArray(array)) {
-        if (array.length !== 0) {
-            let variable = array[0];
-            array.forEach((element) => {
-                if (+variable < +element) {
-                    variable = element;
-                }
-            });
-            return variable;
-        } else return 0;
-    } else return 0;
-};
+    if (!Array.isArray(array)) {
+        return 0;
+    }
+    if (array.length < 1) {
+        return 0;
+    }
 
-exports.max = function max(array) {
-    if (Array.isArray(array)) {
-        if (array.length !== 0) {
-            let variable = array[0];
-            array.forEach((element) => {
-                if (+variable > +element) {
-                    variable = element;
-                }
-            });
-            return variable;
-        } else return 0;
-    } else return 0;
+    let minValue = array[0];
+    array.forEach(element => {
+        if (element < minValue) {
+            minValue = element;
+        }
+    });
+    return minValue;
 };
+// min([1, 3, -5, 7, 0]); // -5
 
-exports.avg = function avg(array) {
-    if (Array.isArray(array)) {
-        if (array.length !== 0) {
-            let sum = 0;
-            array.forEach((element) => {
-                sum += +element;
-            });
-            return sum / array.length;
-        } else return 0;
-    } else return 0;
+
+exports.max = function min(array) {
+    if (!Array.isArray(array)) {
+        return 0;
+    }
+    if (array.length < 1) {
+        return 0;
+    }
+
+    let maxValue = array[0];
+    array.forEach(element => {
+        if (element > maxValue) {
+            maxValue = element;
+        }
+    });
+    return maxValue;
 };
+// max([1, 3, -5, 7, 0]); // 7
+
+
+exports.avg = function min(array) {
+    if (!Array.isArray(array)) {
+        return 0;
+    }
+    if (array.length < 1) {
+        return 0;
+    }
+
+    let sum = array.reduce((accumulator, currentValue) => accumulator + currentValue);
+    return sum / array.length;
+};
+// avg([1, 2, 3, 4]); // 2.5
